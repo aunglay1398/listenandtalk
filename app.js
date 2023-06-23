b02cc5baeb260320fee4fcc5092c07f8f0907e0f
aunglay1398 <101084115+aunglay1398@users.noreply.github.com>
2023-06-23 16:01:39 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const textarea = document.getElementById('text');
+const speakButton = document.getElementById('speak');
+const voicesSelect = document.getElementById('voices');
+const rateInput = document.getElementById('rate');
+
+let voices = [];
+
+function populateVoices() {
+    voices = speechSynthesis.getVoices();
+    voices.forEach((voice, i) => {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = `${voice.name} (${voice.lang})`;
+        voicesSelect.appendChild(option);
+    });
+}
+function stopSpeech() {
+    window.speechSynthesis.cancel();
+}
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+speakButton.addEventListener('click', () => {
+    let text = textarea.value;
+    let utterance = new SpeechSynthesisUtterance(text);
+
+    // Get selected voice
+    let selectedVoiceIndex = voicesSelect.value;
+    utterance.voice = voices[selectedVoiceIndex];
+
+    // Get rate
+    utterance.rate = rateInput.value;
+
+    speechSynthesis.speak(utterance);
+});
